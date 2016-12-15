fb64f54fa63bff999cb276779797931e42b753e2
Matt Szabo <szabo.matthew@gmail.com>
2016-12-15 22:03:37 +1100

    added slots for auto generation

--- index.js
+++ index.js
@@ -5,32 +5,44 @@ app.launch(function(request, response) {
     response.say('hello matt, and hello world');
 });
 
-app.intent('helloIntent',  function(request, response) {
-  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-  var xmlHttp = new XMLHttpRequest();
-  var url;
-  var name = request.slot('name');
+// slots is there to auto generate in schema with `alcl schema`
+app.intent('helloIntent',
+  {
+    "slots": [
+      {
+        "name": "name",
+        "type": "AMAZON.LITERAL"
+      }
+    ]
+    ,"utterances":[ "say the your name" ]
+  },
+  function(request, response) {
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xmlHttp = new XMLHttpRequest();
+    var url;
+    var name = request.slot('name');
 
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      response.say(JSON.parse(xmlHttp.responseText).message);
-      response.send();
+    xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        response.say(JSON.parse(xmlHttp.responseText).message);
+        response.send();
+      }
     }
-  }
-  if( name != undefined) {
-    url = 'http://www.mattcodes.com.au:3903/api/hello?name=' + name;
-  } else {
-    url = 'http://www.mattcodes.com.au:3903/api/hello';
-  }
+    if( name != undefined) {
+      url = 'http://www.mattcodes.com.au:3903/api/hello?name=' + name;
+    } else {
+      url = 'http://www.mattcodes.com.au:3903/api/hello';
+    }
 
-  xmlHttp.open("GET", url, true);
-  xmlHttp.send(null);
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
 
-  // return false to prevent response from automatically sending
-  // in order to wait for the get request,
-  // we manually send the response in onreadystatechange
-  return false;
-});
+    // return false to prevent response from automatically sending
+    // in order to wait for the get request,
+    // we manually send the response in onreadystatechange
+    return false;
+  }
+);
 
 
 if (process.argv.length > 2) {
